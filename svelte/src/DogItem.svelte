97955896c73c4990e0480da41a5c74a436d7e478
unknown <franmolmedo@gmail.com>
2019-08-08 22:33:44 +0200

    Final demo

---
+++ svelte/src/DogItem.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let id;
+  export let name;
+  export let thumbnail;
+</script>
+
+<style>
+  article {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
+
+<article on:click={() => dispatch('selectitem', { id })}>
+  <h1>{name}</h1>
+  <img alt={name} src={thumbnail} />
+</article>
